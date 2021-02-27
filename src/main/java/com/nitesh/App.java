aa81335186e2bc55078fb659ec8656f0f8521634
Nitesh Kumar Modi <niteshkmodi@gmail.com>
2021-02-27 15:12:42 +0530

    maven code

---
+++ src/main/java/com/nitesh/App.java
@@ -0,0 +1,13 @@
+package com.nitesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
